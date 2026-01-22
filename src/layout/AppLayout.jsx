ac2c410d415a6b45e9384b49e79afec987055d91
Tarun-ctrl-ops <tarungopi2712@gmail.com>
2026-01-22 18:43:26 +0530

    added applayout

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -4,7 +4,7 @@ export default function AppLayout() {
   return (
     <>
       <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
-        <Link to="/signup">Signup</Link> |{" "}
+        <Route path="signup" element={<UserPage />} />
         <Link to="/login">Login</Link> |{" "}
         <Link to="/experts">Experts</Link> |{" "}
         <Link to="/availability">Availability</Link> |{" "}
