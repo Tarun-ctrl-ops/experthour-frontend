7cf49b3e8deb5339be88414834ac24d97e7173bb
Tarun-ctrl-ops <tarungopi2712@gmail.com>
2026-01-26 01:02:35 +0530

    added login wiring

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,9 @@
 import api from "./client";
 
 export const register = (data) => api.post("/auth/register", data);
-export const login = (data) =>
-  api.post("/auth/login", data).then(r => r.data);
+export const login = async (data) => {
+  const res = await api.post("/auth/login", data);
+  return res.data.token; // 👈 ONLY return token
+};
+
 
