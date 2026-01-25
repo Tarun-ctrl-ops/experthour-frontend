45a2c95a1d26edcd564eaab8b7efa431f08b2e5e
Tarun-ctrl-ops <tarungopi2712@gmail.com>
2026-01-26 00:38:39 +0530

    fixed backend Auth issue

--- src/api/client.js
+++ src/api/client.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: "https://experthour-backend-1.onrender.com/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
+
